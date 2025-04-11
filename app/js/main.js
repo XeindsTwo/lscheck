a402bd05d9ced0b3a0651bce386f134afbc0f0dd
ArkadiyGenij <agongadze8@gmail.com>
2025-04-11 14:36:54 +0300

    Сделал правки в секциях, улучшил стили. Осталось доделать главный экран (адаптив)

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,15 @@
 import {header} from './header.js';
 import {mechanism} from './mechanism.js';
-import {footer} from './footer.js';
 
 header();
 mechanism();
-footer();
 
 new Swiper('.swiper', {
   slidesPerView: 1,
+  spaceBetween: 30,
   loop: true,
   autoHeight: true,
   speed: 600,
-  allowTouchMove: false,
   navigation: {
     prevEl: '.cases__btn--prev',
     nextEl: '.cases__btn--next'
--- gulpfile.js
+++ gulpfile.js
@@ -12,121 +12,120 @@ const replace = require('gulp-replace');
 const fileInclude = require('gulp-file-include');
 
 const htmlInclude = () => {
-    return src(['app/html/*.html'])
-        .pipe(fileInclude({
-            prefix: '@',
-            basepath: '@file',
-        }))
-        .pipe(dest('app'))
-        .pipe(browserSync.stream());
+  return src(['app/html/*.html'])
+      .pipe(fileInclude({
+        prefix: '@',
+        basepath: '@file',
+      }))
+      .pipe(dest('app'))
+      .pipe(browserSync.stream());
 }
 
 function svgSprites() {
-    return src('app/images/icons/*.svg')
-        .pipe(cheerio({
-                run: ($) => {
-                    $("[fill]").removeAttr("fill");
-                    $("[stroke]").removeAttr("stroke");
-                    $("[style]").removeAttr("style");
-                },
-                parserOptions: {xmlMode: true},
-            })
-        )
-        .pipe(replace('&gt;', '>'))
-        .pipe(
-            svgSprite({
-                mode: {
-                    stack: {
-                        sprite: '../sprite.svg',
-                    },
-                },
-            })
-        )
-        .pipe(dest('app/images'));
+  return src('app/images/icons/*.svg')
+      .pipe(cheerio({
+            run: ($) => {
+              $("[fill]").removeAttr("fill");
+              $("[stroke]").removeAttr("stroke");
+              $("[style]").removeAttr("style");
+            },
+            parserOptions: {xmlMode: true},
+          })
+      )
+      .pipe(replace('&gt;', '>'))
+      .pipe(
+          svgSprite({
+            mode: {
+              stack: {
+                sprite: '../sprite.svg',
+              },
+            },
+          })
+      )
+      .pipe(dest('app/images'));
 }
 
 
 function images() {
-    return src('app/images/**/*.*')
-        .pipe(imagemin([
-            imagemin.gifsicle({
-                interlaced: true
-            }),
-            imagemin.mozjpeg({
-                quality: 75,
-                progressive: true
-            }),
-            imagemin.optipng({
-                optimizationLevel: 5
-            }),
-            imagemin.svgo({
-                plugins: [{
-                    removeViewBox: true
-                },
-                    {
-                        cleanupIDs: false
-                    }
-                ]
-            })
-        ]))
-        .pipe(dest('dist/images'))
+  return src('app/images/**/*.*')
+      .pipe(imagemin([
+        imagemin.gifsicle({
+          interlaced: true
+        }),
+        imagemin.mozjpeg({
+          quality: 75,
+          progressive: true
+        }),
+        imagemin.optipng({
+          optimizationLevel: 5
+        }),
+        imagemin.svgo({
+          plugins: [{
+            removeViewBox: true
+          },
+            {
+              cleanupIDs: false
+            }
+          ]
+        })
+      ]))
+      .pipe(dest('dist/images'))
 }
 
 function browsersync() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        },
-        notify: false
-    });
+  browserSync.init({
+    server: {
+      baseDir: "app/"
+    },
+    notify: false
+  });
 }
 
 function clearDist() {
-    return del('dist')
+  return del('dist')
 }
 
 function scripts() {
-    return src([
-        'node_modules/swiper/swiper-bundle.min.js',
-        'app/js/main.js'
-    ])
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+  return src([
+    'app/js/main.js'
+  ])
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+      .pipe(dest('app/js'))
+      .pipe(browserSync.stream())
 }
 
 function styles() {
-    return src('app/scss/style.scss')
-        .pipe(scss({
-            outputStyle: 'compressed'
-        }))
-        .pipe(concat('style.min.css'))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 10 version'],
-            grid: true
-        }))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream())
+  return src('app/scss/style.scss')
+      .pipe(scss({
+        outputStyle: 'compressed'
+      }))
+      .pipe(concat('style.min.css'))
+      .pipe(autoprefixer({
+        overrideBrowserslist: ['last 10 version'],
+        grid: true
+      }))
+      .pipe(dest('app/css'))
+      .pipe(browserSync.stream())
 }
 
 function watching() {
-    watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/html/**/*.html'], htmlInclude);
-    watch(['app/images/icons/*.svg'], svgSprites);
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/html/**/*.html'], htmlInclude);
+  watch(['app/images/icons/*.svg'], svgSprites);
 }
 
 function build() {
-    return src([
-        'app/css/style.min.css',
-        'app/fonts/**/*',
-        'app/js/main.min.js',
-        'app/*.html'
-    ], {
-        base: 'app'
-    })
-        .pipe(dest('dist'))
+  return src([
+    'app/css/style.min.css',
+    'app/fonts/**/*',
+    'app/js/main.min.js',
+    'app/*.html'
+  ], {
+    base: 'app'
+  })
+      .pipe(dest('dist'))
 }
 
 exports.styles = styles;
